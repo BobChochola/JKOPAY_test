dde6554ab398c5e0afa0b9c77313fde0c7dd128e
Bob Chen <bob148666@gmail.com>
2023-06-12 19:05:57 +0800

    login test function

---
+++ tests/test_login.py
@@ -0,0 +1,90 @@
+import requests
+import pytest
+
+def api_url():
+    return "https://api.example/service/login"
+
+def test_login_missing_fields(api_url):
+
+   account = "valid_account"
+
+    payload = {
+        "Account": account
+        # login_auth = "miss auth"
+    }
+
+    response = requests.post(api_url, json=payload)
+
+    assert response.status_code == 400
+    
+    result = response.json()
+    assert result["Status"] == "ErrorCode_02" # 400
+    assert result["Message"] == "提示錯誤訊息 02" #login_auth is required, or Missing required parameters
+
+def test_login_invalid_credentials(api_url):
+
+    account = "invalid_account" #bob@gmail.com not exist, bob@123, 123@123
+    login_auth = "invalid_login_auth"
+    datetime = "2023-06-12"
+
+    payload = {
+        "Account": account,
+        "LoginAuth": login_auth,
+        "Datetime": datetime
+    }
+
+    response = requests.post(api_url, json=payload)
+
+    assert response.status_code == 401
+
+    result = response.json()
+    assert result["Status"] == "ErrorCode_01" #401
+    assert result["Message"] == "提示錯誤訊息 01" #account or login_auth are invalid
+
+    def test_login_subscription_failed(api_url):
+    account = "example_account"
+    login_auth = "example_invalid_auth"
+    payload = {
+        "Account": account,
+        "LoginAuth": login_auth
+    }
+    response = requests.post(api_url, json=payload)
+    assert response.status_code == 402
+    
+    result = response.json()
+    assert result["Status"] == 402
+    assert result["Message"] == "登入驗證失敗"
+
+
+def test_login_invalid_request(api_url):
+
+    payload = {
+        "Account": "valid_account",
+        "LoginAuth": "valid_auth"
+    }
+
+    response = requests.get(api_url, json=payload)
+
+    assert response.status_code == 405 
+    result = response.json()
+    assert result["Status"] == "ErrorCode_03" # 405
+    assert result["Message"] == "提示錯誤訊息 03" #this api can not support .get
+
+@pytest.mark.parametrize("parameter_name, parameter_value, max_length", [
+    ("Account", "example_account", 20),
+    ("LoginAuth", "example_login_auth", 36),
+    ("Datetime", "2023-06-12", 20)
+])
+def test_parameter_max_length(api_url, parameter_name, parameter_value, max_length):
+    parameter_value = parameter_value * (max_length + 1)
+    
+    payload = {
+        parameter_name: parameter_value
+    }
+    response = requests.post(api_url, json=payload)
+    
+    assert response.status_code == 400
+    
+    result = response.json()
+    assert result["Status"] == "ErrorCode_02" # 400
+    assert result["Message"] == "提示錯誤訊息 02" #login_auth is required, or Missing required parameters or "over max length"
---
+++ tests/test_login_api.py
@@ -0,0 +1,94 @@
+import requests
+import pytest
+
+def api_url():
+    return "https://api.example/service/login"
+
+def test_login_successful(api_url):
+    account = "valid_account" # #bob@gmail.com
+    login_auth = "valid_login_auth"
+    datetime = "2023-06-12"
+    
+    payload = {
+        "Account": account,
+        "LoginAuth": login_auth,
+        "Datetime": datetime
+    }
+    response = requests.post(api_url, json=payload)
+    
+    assert response.status_code == 200
+    
+    result = response.json()
+    assert result["Status"] == 200
+    assert result["Message"] == "Login successful"
+
+    def test_account_number(api_url):
+    account = 12345
+    login_auth = "example_login_auth"
+    payload = {
+        "Account": str(account),
+        "LoginAuth": login_auth
+    }
+    response = requests.post(api_url, json=payload)
+    assert response.status_code == 200
+    
+    result = response.json()
+    assert result["Status"] == 200
+    assert result["Message"] == "Login successful"
+
+def test_login_less_datatime_successful(api_url):
+    account = "valid_account"
+    login_auth = "valid_login_auth"
+    
+    payload = {
+        "Account": account,
+        "LoginAuth": login_auth,
+    }
+    response = requests.post(api_url, json=payload)
+    
+    assert response.status_code == 200
+    
+    result = response.json()
+    assert result["Status"] == 200
+    assert result["Message"] == "Login successful"
+
+    def test_datetime_invalid_successful(api_url):
+    account = "example_account"
+    login_auth = "example_login_auth"
+    datetime = "abc123"
+    
+    payload = {
+        "Account": account,
+        "LoginAuth": login_auth,
+        "Datetime": datetime
+    }
+    response = requests.post(api_url, json=payload)
+    
+    assert response.status_code == 200
+    
+    result = response.json()
+    assert result["Status"] == 200
+    assert result["Message"] == "Login successful"
+
+    import requests
+import pytest
+
+@pytest.fixture
+def api_url():
+    return "https://api.example/service/login"
+
+def test_endpoint_not_found(api_url):
+    # 準備測試資料，模擬 API 端點不存在的情境
+    invalid_endpoint = "https://api.example/service/invalid_endpoint"
+    account = "example_account"
+    login_auth = "example_login_auth"
+    
+    # 發送 POST 請求到不存在的 API 端點
+    payload = {
+        "Account": account,
+        "LoginAuth": login_auth
+    }
+    response = requests.post(invalid_endpoint, json=payload)
+    
+    # 驗證回應的狀態碼
+    assert response.status_code == 404
